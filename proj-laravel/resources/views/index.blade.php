3a227f3def99a14a15086bf61d41fb8ce18c58e0
aminekefi <aminekefi045@gmail.com>
2022-05-20 21:36:06 +0100

    creation controller tabs bd

---
+++ proj-laravel/resources/views/index.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>LMS</title>
+        <link type="text/css" href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="./bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600'
+            rel='stylesheet'>
+
+
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+            <!-- CSS only -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="index.html">Home </a>
+                    <div class="nav-collapse collapse navbar-inverse-collapse">
+                        <ul class="nav pull-right">
+                            <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="images/user.png" class="nav-avatar" />
+                                <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="index.html">Your Profile</a></li>
+                                    <!--li><a href="#">Edit Profile</a></li>
+                                    <li><a href="#">Account Settings</a></li-->
+                                    <li class="divider"></li>
+                                    <li><a href="logout.html">Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /.nav-collapse -->
+                </div>
+            </div>
+            <!-- /navbar-inner -->
+        </div>
+        <!-- /navbar -->
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="span3">
+                        <div class="sidebar">
+                            <ul class="widget widget-menu unstyled">
+                                <li class="active"><a href="index.html"><i class="menu-icon icon-home"></i>Home
+                                </a></li>
+                                 <li><a href="message.html"><i class="menu-icon icon-inbox"></i>Messages</a>
+                                </li>
+                                <li><a href="student.html"><i class="menu-icon icon-user"></i>Manage Students </a>
+                                </li>
+                                <li><a href="book.html"><i class="menu-icon icon-book"></i>All Books </a></li>
+                                <li><a href="addbook.html"><i class="menu-icon icon-edit"></i>Add Books </a></li>
+                                <li><a href="requests.html"><i class="menu-icon icon-tasks"></i>Issue/Return Requests </a></li>
+                                <li><a href="recommendations.html"><i class="menu-icon icon-list"></i>Book Recommendations </a></li>
+                                <li><a href="current.html"><i class="menu-icon icon-list"></i>Currently Issued Books </a></li>
+                            </ul>
+                            <ul class="widget widget-menu unstyled">
+                                <li><a href="logout.html"><i class="menu-icon icon-signout"></i>Logout </a></li>
+                            </ul>
+                        </div>
+                        <!--/.sidebar-->
+                    </div>
+                    <!--/.span3-->
+
+                    <div class="span9">
+                        <center>
+                            <div class="card" style="width: 50%;">
+                                <img class="card-img-top" src="images/profile2.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <i>
+                                        <h1 class="card-title"><center></center></h1>
+                                        <br>
+                                        <p><b>Email ID: </b></p>
+                                        <br>
+                                        <p><b>Mobile number: </b></p>
+                                        </b>
+                                    </i>
+
+                                    </div>
+                                </div>
+                            <br>
+                            <a href="edit_admin.html" class="btn btn-primary">Edit Details</a>
+                            </center>
+                        </div>
+
+                        <!--/.span9-->
+                    </div>
+                </div>
+                <!--/.container-->
+            </div>
+    <div class="footer">
+                <div class="container">
+
+                </div>
+            </div>
+
+            <!--/.wrapper-->
+            <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+            <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+            <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+            <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+            <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+            <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
+            <script src="scripts/common.js" type="text/javascript"></script>
+
+        </body>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    </html>
